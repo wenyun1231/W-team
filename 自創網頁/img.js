e6b68e65efd1251e4841cdc605eadc3f0dec5fbe
wenyun1231 <ree58200@gmail.com>
2021-07-29 11:26:39 +0800

    隱藏選單修改與音樂

--- 自創網頁/img.js
+++ 自創網頁/img.js
@@ -1,9 +1,7 @@
-$('.bg11').slick({
-    arrows:true,/*顯示箭頭*/
-    slidesToShow:1,/*播放一張圖*/
-    slidesToScroll:1,/*滑動張數*/
-    autoplay:true,/*自動播放*/
-    autoplaySpeed:4000,/*播放速度*/
+$('.music').slick({
+    dots:true,
+    slidesToShow:1,
+    slidesToScroll:1,
 });
 $('.bg21').slick({
     arrows:true,
